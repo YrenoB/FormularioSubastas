d6cfeabf73e0987e57997c0f6ec48a1a19cea1e8
zreno-boop <zvelasquezb@renobo.com.co>
2025-11-16 16:46:55 -0500

    Ya medio sirve

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3001,
-    proxy: {
-      "/api/": "http://localhost:3000/",
-    },
+    port: 5173,
   },
 })
